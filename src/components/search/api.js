759e4dc06f47c95dc7a47f06aa5bd8faa594cf13
anpanring <jid9834@nyu.edu>
2023-09-28 13:51:12 -0400

    initial upload

---
+++ src/components/search/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3a260fca38msh398a6d8aa5b592dp1c277ejsn821e6d629ee3',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "a49b4b76a6cc4f799942932af7df1757";
